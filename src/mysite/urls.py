015e65a63f1eb7c3cb04c8fefbda668dfe50dffb
CagyLocket <pawel.wrzesien@icloud.com>
2021-12-08 21:53:30 +0100

    Register view, model, template created

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -22,10 +22,19 @@ from personal.views import (
 	home_screen_view
 )
 
+from account.views import (
+    register_view,
+
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_screen_view, name="home")
+    path('', home_screen_view, name="home"),
+    path('register/', register_view, name="register")
+    
 ]
+
 if settings.DEBUG:
 	urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 	urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
