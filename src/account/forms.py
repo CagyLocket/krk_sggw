a4750afb215c7be9c6f3fb5904e815d1ef13e99b
CagyLocket <pawel.wrzesien@icloud.com>
2021-12-15 16:59:52 +0100

    uploading profile image

--- src/account/forms.py
+++ src/account/forms.py
@@ -101,5 +101,6 @@ class AccountUpdateForm(forms.ModelForm):
 
 
 
+
 		
 
--- src/account/views.py
+++ src/account/views.py
@@ -136,8 +136,9 @@ def edit_account_view(request, *args, **kwargs):
 	if request.POST:
 		form = AccountUpdateForm(request.POST, request.FILES, instance=request.user)
 		if form.is_valid():
+			# delete the old profile image so the name is preserved
+			account.profile_image.delete()
 			form.save()
-			new_username = form.cleaned_data['username']
 			return redirect("account:view", user_id=account.pk)
 		else:
 			form = AccountUpdateForm(request.POST, instance=request.user,
