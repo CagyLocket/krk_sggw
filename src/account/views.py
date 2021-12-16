b1a20d7130ed4c2275eb1660b2a39f646b31a8f9
CagyLocket <pawel.wrzesien@icloud.com>
2021-12-16 14:49:17 +0100

    crop image addded but still doesnt work

--- src/account/views.py
+++ src/account/views.py
@@ -1,19 +1,47 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.models import User
 from django.conf import settings
 
+from django.core.files.storage import default_storage
+from django.core.files.storage import FileSystemStorage
+import os
+import cv2
+import json
+import base64
+import requests
+from django.core import files
+
+
 from account.forms import RegistrationForm, AccountAuthenticationForm, AccountUpdateForm
-from account.models import Account 
+from account.models import Account
+
+
+TEMP_PROFILE_IMAGE_NAME = "temp_profile_image.png"
+
+# This is basically almost exactly the same as friends/friend_list_view
+def account_search_view(request, *args, **kwargs):
+	context = {}
+	if request.method == "GET":
+		search_query = request.GET.get("q")
+		if len(search_query) > 0:
+			search_results = Account.objects.filter(email__icontains=search_query).filter(username__icontains=search_query).distinct()
+			user = request.user
+			accounts = [] # [(account1, True), (account2, False), ...]
+			for account in search_results:
+				accounts.append((account, False)) # you have no friends yet
+			context['accounts'] = accounts
+				
+	return render(request, "account/search_results.html", context)
+
 
 
 def register_view(request, *args, **kwargs):
 	user = request.user
-	if user.is_authenticated:
-		return HttpResponse(f"You are already authenticated as {user.email}.")
-	context = {}
+	if user.is_authenticated: 
+		return HttpResponse("You are already authenticated as " + str(user.email))
 
+	context = {}
 	if request.POST:
 		form = RegistrationForm(request.POST)
 		if form.is_valid():
@@ -22,19 +50,17 @@ def register_view(request, *args, **kwargs):
 			raw_password = form.cleaned_data.get('password1')
 			account = authenticate(email=email, password=raw_password)
 			login(request, account)
-			destination = get_redirect_if_exists(request)
+			destination = kwargs.get("next")
 			if destination:
 				return redirect(destination)
-			return redirect("home")
+			return redirect('home')
 		else:
 			context['registration_form'] = form
-	
-	else:
-			form = RegistrationForm()
-			context['registration_form'] = form
-	
-	return render(request, 'account/register.html', context)
 
+	else:
+		form = RegistrationForm()
+		context['registration_form'] = form
+	return render(request, 'account/register.html', context)
 
 
 def logout_view(request):
@@ -43,12 +69,14 @@ def logout_view(request):
 
 
 def login_view(request, *args, **kwargs):
-
 	context = {}
 
 	user = request.user
-	if user.is_authenticated:
+	if user.is_authenticated: 
 		return redirect("home")
+
+	destination = get_redirect_if_exists(request)
+	print("destination: " + str(destination))
 
 	if request.POST:
 		form = AccountAuthenticationForm(request.POST)
@@ -56,34 +84,46 @@ def login_view(request, *args, **kwargs):
 			email = request.POST['email']
 			password = request.POST['password']
 			user = authenticate(email=email, password=password)
+
 			if user:
 				login(request, user)
-				destination = get_redirect_if_exists(request)
 				if destination:
 					return redirect(destination)
 				return redirect("home")
-		else:
-			context['login_form'] = form 
+
+	else:
+		form = AccountAuthenticationForm()
+
+	context['login_form'] = form
+
 	return render(request, "account/login.html", context)
 
 
-
-
 def get_redirect_if_exists(request):
-	redirect= None
+	redirect = None
 	if request.GET:
 		if request.GET.get("next"):
 			redirect = str(request.GET.get("next"))
-	return redirect 
+	return redirect
+
+
 
 
 def account_view(request, *args, **kwargs):
+	"""
+	- Logic here is kind of tricky
+		is_self (boolean)
+			is_friend (boolean)
+				-1: NO_REQUEST_SENT
+				0: THEM_SENT_TO_YOU
+				1: YOU_SENT_TO_THEM
+	"""
 	context = {}
 	user_id = kwargs.get("user_id")
 	try:
 		account = Account.objects.get(pk=user_id)
-	except Account.DoesNotExist:
-		return HttpResponse("That user doesn't exist.")
+	except:
+		return HttpResponse("Something went wrong.")
 	if account:
 		context['id'] = account.id
 		context['username'] = account.username
@@ -91,7 +131,7 @@ def account_view(request, *args, **kwargs):
 		context['profile_image'] = account.profile_image.url
 		context['hide_email'] = account.hide_email
 
-		# Define state template veriables
+		# Define template variables
 		is_self = True
 		is_friend = False
 		user = request.user
@@ -99,30 +139,75 @@ def account_view(request, *args, **kwargs):
 			is_self = False
 		elif not user.is_authenticated:
 			is_self = False
+			
+		# Set the template variables to the values
 		context['is_self'] = is_self
 		context['is_friend'] = is_friend
 		context['BASE_URL'] = settings.BASE_DIR
-
 		return render(request, "account/account.html", context)
 
+def save_temp_profile_image_from_base64String(imageString, user):
+	INCORRECT_PADDING_EXCEPTION = "Incorrect padding"
+	try:
+		if not os.path.exists(settings.TEMP):
+			os.mkdir(settings.TEMP)
+		if not os.path.exists(settings.TEMP + "/" + str(user.pk)):
+			os.mkdir(settings.TEMP + "/" + str(user.pk))
+		url = os.path.join(settings.TEMP + "/" + str(user.pk),TEMP_PROFILE_IMAGE_NAME)
+		storage = FileSystemStorage(location=url)
+		image = base64.b64decode(imageString)
+		with storage.open('', 'wb+') as destination:
+			destination.write(image)
+			destination.close()
+		return url
+	except Exception as e:
+		print("exception: " + str(e))
+		# workaround for an issue I found
+		if str(e) == INCORRECT_PADDING_EXCEPTION:
+			imageString += "=" * ((4 - len(imageString) % 4) % 4)
+			return save_temp_profile_image_from_base64String(imageString, user)
+	return None
 
-def account_search_view(request, *args, **kwargs):
-	context = {}
 
-	if request.method == "GET":
-		search_query = request.GET.get("q")
-		if len(search_query) > 0:
-			search_results = Account.objects.filter(email__icontains=search_query).filter(
-				username__icontains=search_query).distinct()
-			user = request.user
-			accounts = [] # [(account1, True), (account2, False), ...]
-			for account in search_results:
-				accounts.append((account, False)) # you have no friends
-			context['accounts'] = accounts
+def crop_image(request, *args, **kwargs):
+	payload = {}
+	user = request.user
+	if request.POST and user.is_authenticated:
+		try:
+			imageString = request.POST.get("image")
+			url = save_temp_profile_image_from_base64String(imageString, user)
+			img = cv2.imread(url)
 
-	return render(request, "account/search_results.html", context)
+			cropX = int(float(str(request.POST.get("cropX"))))
+			cropY = int(float(str(request.POST.get("cropY"))))
+			cropWidth = int(float(str(request.POST.get("cropWidth"))))
+			cropHeight = int(float(str(request.POST.get("cropHeight"))))
+			if cropX < 0:
+				cropX = 0
+			if cropY < 0: # There is a bug with cropperjs. y can be negative.
+				cropY = 0
+			crop_img = img[cropY:cropY+cropHeight, cropX:cropX+cropWidth]
 
+			cv2.imwrite(url, crop_img)
 
+			# delete the old image
+			user.profile_image.delete()
+
+			# Save the cropped image to user model
+			user.profile_image.save("profile_image.png", files.File(open(url, 'rb')))
+			user.save()
+
+			payload['result'] = "success"
+			payload['cropped_profile_image'] = user.profile_image.url
+
+			# delete temp file
+			os.remove(url)
+			
+		except Exception as e:
+			print("exception: " + str(e))
+			payload['result'] = "error"
+			payload['exception'] = str(e)
+	return HttpResponse(json.dumps(payload), content_type="application/json")
 
 
 def edit_account_view(request, *args, **kwargs):
@@ -136,8 +221,6 @@ def edit_account_view(request, *args, **kwargs):
 	if request.POST:
 		form = AccountUpdateForm(request.POST, request.FILES, instance=request.user)
 		if form.is_valid():
-			# delete the old profile image so the name is preserved
-			account.profile_image.delete()
 			form.save()
 			return redirect("account:view", user_id=account.pk)
 		else:
@@ -164,9 +247,6 @@ def edit_account_view(request, *args, **kwargs):
 		context['form'] = form
 	context['DATA_UPLOAD_MAX_MEMORY_SIZE'] = settings.DATA_UPLOAD_MAX_MEMORY_SIZE
 	return render(request, "account/edit_account.html", context)
-
-
-
 
 
 
